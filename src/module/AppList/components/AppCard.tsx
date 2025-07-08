85489f8f8b659a4ecfc61bf71d9d87c232bdf367
DanielSerio <dserio.d@gmail.com>
2025-07-08 07:28:47 -0400

    fixed card images

--- src/module/AppList/components/AppCard.tsx
+++ src/module/AppList/components/AppCard.tsx
@@ -1,24 +1,16 @@
 import type { AppLinkType } from "#const";
 import { Box, Card, Text } from "@mantine/core";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 
 export function AppCard({ href, imgSrc, title, description }: AppLinkType) {
-  const [image, setImage] = useState<string | null>(null);
-
-  useEffect(() => {
-    import(`../../../assets${imgSrc}`).then((res) => {
-      setImage(res.default);
-    });
-  }, [imgSrc]);
-
   const isPokeApi = useMemo(() => imgSrc?.includes("pokeapi"), [imgSrc]);
 
   return (
     <Card className="app-card" key={href} h="100%">
       <Card.Section className="header" component="a" href={`/#/apps${href}`}>
-        {!!image ? (
+        {!!imgSrc ? (
           <figure className={isPokeApi ? "poke" : undefined}>
-            <img src={image} />
+            <img src={imgSrc} />
           </figure>
         ) : (
           "Loading..."
@@ -31,7 +23,6 @@ export function AppCard({ href, imgSrc, title, description }: AppLinkType) {
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            width: "fit-content",
             background: "black",
             borderRadius: "5px",
           }}
