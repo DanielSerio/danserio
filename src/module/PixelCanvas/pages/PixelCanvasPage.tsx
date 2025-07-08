4e748214f804be987f2d17218b7aa6777e792f18
DanielSerio <dserio.d@gmail.com>
2025-07-08 07:08:18 -0400

    center canvas/pixel canvas form

--- src/module/PixelCanvas/pages/PixelCanvasPage.tsx
+++ src/module/PixelCanvas/pages/PixelCanvasPage.tsx
@@ -2,13 +2,17 @@ import { AppBreadcrumbs } from "#core/components";
 import { PixelCanvas } from "#pixel-canvas/components/PixelCanvas/PixelCanvas";
 import { PixelCanvasForm } from "#pixel-canvas/components/PixelCanvas/PixelCanvasForm";
 import { PixelCanvasProvider } from "#pixel-canvas/hooks/usePixelCanvas";
+import "#pixel-canvas/styles/output/pixel-canvas.scss";
+import { Box } from "@mantine/core";
 
 export function PixelCanvasPage() {
   return (
     <PixelCanvasProvider>
       <AppBreadcrumbs />
-      <PixelCanvas />
-      <PixelCanvasForm />
+      <Box p="md" className="workspace">
+        <PixelCanvas />
+        <PixelCanvasForm />
+      </Box>
     </PixelCanvasProvider>
   );
 }
