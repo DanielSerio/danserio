70680a73acdc90b86bf63c70fa661fea3d98f0b9
DanielSerio <dserio.d@gmail.com>
2025-07-08 07:52:26 -0400

    added particles background hook

--- src/module/About/pages/AboutPage.tsx
+++ src/module/About/pages/AboutPage.tsx
@@ -1,12 +1,20 @@
 import { AboutMe } from "#about/components/AboutMe";
+import { ParticlesBackground } from "#core/components/layout/ParticlesBackground";
+import { useParticles } from "#core/hooks";
 import { Button } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 
 export function AboutPage() {
   const navigate = useNavigate();
+  const [{ options }, { onParticlesLoaded }] = useParticles({});
 
   return (
     <>
+      <ParticlesBackground
+        id="aboutParticles"
+        options={options}
+        particlesLoaded={onParticlesLoaded}
+      />
       <AboutMe>
         <Button onClick={() => navigate("/apps")}>See some mini-apps</Button>
       </AboutMe>
