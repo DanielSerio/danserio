9b0e111cf52ed4a133631772e68360950dfc4fcc
DanielSerio <dserio.d@gmail.com>
2025-07-08 12:29:35 -0400

    setup for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/danserio/",
   resolve: {
     alias: {
       "#const": resolve(__dirname, 'src/const'),
